6556874f0ac6223e3d87ce49b255bb988b9e5aa8
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-02 16:00:08 +0000

    Set initial state for a single cocktail page

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -7,12 +7,15 @@ function SearchForm() {
   const searchCocktail = () => {
     setSearchTerm(searchValue.current.value);
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   useEffect(() => {
     searchValue.current.focus();
   }, []);
   return (
     <section className="section search">
-      <form action="" className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">Search your favorite cocktail</label>
           <input
--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function SingleCocktail() {
-  return <div>Single Cocktail</div>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [cocktail, setCocktail] = useState(null);
+  return <div></div>;
 }
 
 export default SingleCocktail;
