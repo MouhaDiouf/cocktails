d27d2fe34558d8a14c258ad607537873748a2813
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-02 15:05:14 +0000

    Style the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 function About() {
   return (
-    <div>
-      <h2>About Us</h2>
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, repellat
         dignissimos quod facilis quis, eaque ullam ipsum reprehenderit iste
         error consectetur necessitatibus nulla voluptatibus amet impedit beatae
         delectus maiores aliquam!
       </p>
-    </div>
+    </section>
   );
 }
 
