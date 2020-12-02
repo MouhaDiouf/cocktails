f4e03f907ffde04ebcfc6bf91c13e131c8b9050a
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-02 16:11:45 +0000

    Destructure the info from a single cocktail

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,10 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 function SingleCocktail() {
   const { id } = useParams();
   const [loading, setLoading] = useState(false);
   const [cocktail, setCocktail] = useState(null);
+  useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const response = await fetch(`${url}${id}`);
+        const data = await response.json();
+        const { drinks } = data;
+        if (drinks) {
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = drinks[0];
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          };
+          setCocktail(newCocktail);
+        } else {
+          setCocktail(null);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }
+    getCocktail();
+  }, [id]);
   return <div></div>;
 }
 
