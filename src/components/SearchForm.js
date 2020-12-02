5a922d469073d5ffa56091df53b08386f23d4e1d
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-02 15:53:24 +0000

    Use the UseRef hook to focus on the search input

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useGlobalContext } from '../context';
 
 function SearchForm() {
   const { setSearchTerm } = useGlobalContext();
-
-  return <div>Search Form</div>;
+  const searchValue = useRef('');
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+  return (
+    <section className="section search">
+      <form action="" className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">Search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default SearchForm;
