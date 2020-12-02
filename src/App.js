6e7cc69d8a15fcef6f25fb417ea523020da81cd0
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-02 14:47:26 +0000

    Create the SingleCocktail page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { Router } from 'react-router-dom';
+import { Route, Router, Switch } from 'react-router-dom';
 import './App.css';
+import About from './pages/About';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cocktail/:id"></Route>
+      </Switch>
     </Router>
   );
 }
